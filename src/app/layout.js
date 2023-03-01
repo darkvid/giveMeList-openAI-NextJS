70178d15553eee67af1343b9d4345e6f9d76cc03
David Galisteo Cantero <darkvidvsgalis@gmail.com>
2023-03-01 06:39:52 +0100

    fix styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 //import './globals.css'
+import '../styles/mvp.css'
 
 import NavMenu from "@/components/navMenu";
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import ListForm from '@/components/ListForm'
 import { Inter } from '@next/font/google'
-import '../styles/mvp.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
